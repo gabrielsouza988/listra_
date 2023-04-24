cbf9f4a12addf2fe743237583976ca36cb1b0059
Gabriel <gabriel@jltecno.com.br>
2023-04-23 23:52:15 -0300

    ajustes no seeder

--- laravel/database/seeders/VehicleSeeder.php
+++ laravel/database/seeders/VehicleSeeder.php
@@ -16,25 +16,6 @@ class VehicleSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\Vehicle::factory(1)->create();
-        $faker = Faker::create();
-        $response = Http::get('https://vpic.nhtsa.dot.gov/api/vehicles/GetVehicleTypesForMake/merc?format=json');
-        $vehicles = $response->json();
-        $values = [];
-        foreach ($vehicles['Results'] as $key => $vehicle) {
-            $values[] = [
-                'foto' => $faker->imageUrl(),
-                'cidade' => $faker->city(),
-                'marca' => $vehicle['MakeName'],
-                'modelo' => $vehicle['MakeName'],
-                'descrição' => $faker->text(100),
-                'ano' => $faker->year,
-                'quilometragem' => $faker->numerify(),
-                'tipo' => $faker->randomElement($array = array('Automático', 'Manual')),
-                'telefone' => $faker->phoneNumber,
-                'valor' => $faker->randomNumber(6) / 100
-            ];
-        }
-        \DB::table('vehicles')->insert($values);
+        Vehicle::factory(10)->create();
     }
 }
